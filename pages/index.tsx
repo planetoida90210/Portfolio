2eba4d72ea025731a728648730d92195fd281d1f
michal <galeriakoloru90210@gmail.com>
2022-09-18 14:56:59 +0200

    implement hero, react-sipmle-typewriter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 
 const Home: NextPage = () => {
@@ -11,7 +12,9 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       {/* Hero */}
-
+      <section id="hero">
+        <Hero />
+      </section>
       {/* About */}
 
       {/* Experience */}
