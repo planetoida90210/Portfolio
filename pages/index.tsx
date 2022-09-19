28f408e65a2fd800f4f70d2e33f83b4221cc4e3f
michal <galeriakoloru90210@gmail.com>
2022-09-19 22:43:51 +0200

    boilerplate for experience tab

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import WorkExperience from '../components/WorkExperience'
 
 
 const Home: NextPage = () => {
@@ -12,16 +13,16 @@ const Home: NextPage = () => {
         <title>Michał Bryk - Porfolio</title>
       </Head>
       <Header />
-      {/* Hero */}
       <section id="hero" className="snap-start">
         <Hero />
       </section>
-      {/* About */}
       <section id="about" className="snap-center">
         <About />
       </section>
       {/* Experience */}
-
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
       {/* Skills */}
 
       {/* Projects */}
