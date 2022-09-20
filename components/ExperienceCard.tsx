abf1208eef0ed4c2b81a0089efc5428d2a1b1211
michal <galeriakoloru90210@gmail.com>
2022-09-20 23:16:19 +0200

    bolierplate for experience card

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 
 type Props = {}
 
 export default function ExperienceCard({}: Props) {
   return (
-    <div>ExperienceCard</div>
+    <article>
+        <motion.img 
+         initial={{ y: -100,opacity: 0 }}
+         transition={{ duration: 1.2 }}
+         whileInView={{ opacity: 1,y: 0 }}
+         viewport={{ once: true }}
+         className="w-32 h-32 rounded-full md:rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+         src="https://avatars.githubusercontent.com/u/47723901?v=4"
+         alt="experience hero image"
+        />
+
+        <div>
+            <h4>Web Developer</h4>
+            <p>MICROSOFT</p>
+            <div>
+
+            </div>
+            <p>Started work ... Ended....</p>
+            <ul>
+                <li>obowiazki</li>
+                <li>obowiazki</li>
+                <li>obowiazki</li>
+                <li>obowiazki</li>
+                <li>obowiazki</li>
+            </ul>
+        </div>
+    </article>
   )
 }
