7076b8cf0c6d2ef8a0e1475914a55f1b55a070ed
michal <galeriakoloru90210@gmail.com>
2022-09-25 01:11:06 +0200

    create contact me, add css to projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import About from '../components/About'
+import ContactMe from '../components/ContactMe'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Projects from '../components/Projects'
@@ -30,7 +31,9 @@ const Home: NextPage = () => {
       <section id="projects" className="snap-start">
         <Projects />
       </section>
-      {/* Contact */}
+      <section>
+        <ContactMe />
+      </section>
     </div>
   )
 }
