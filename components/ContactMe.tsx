ce3d8fed2cee588ecfe970f926584378863b4918
michal <galeriakoloru90210@gmail.com>
2022-09-26 20:36:38 +0200

    implement template contact email structure

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -13,7 +13,9 @@ type Props = {}
 
 export default function ContactMe({}: Props) {
   const { register, handleSubmit } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData)
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:michalbryk1991@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`
+  };
 
   return (
     <div className="h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
@@ -41,11 +43,11 @@ export default function ContactMe({}: Props) {
         </div>
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
             <div className="flex space-x-2">
-                <input placeholder="Name" className="contactInput" type="text" />
-                <input placeholder="Email" className="contactInput" type="email" />
+                <input {...register('name')} placeholder="Name" className="contactInput" type="text" />
+                <input {...register('email')} placeholder="Email" className="contactInput" type="email" />
             </div>
-            <input placeholder="Subject" className="contactInput" type="text" />
-            <textarea placeholder="Message" className="contactInput" />
+            <input {...register('subject')} placeholder="Subject" className="contactInput" type="text" />
+            <textarea {...register('message')} placeholder="Message" className="contactInput" />
             <button type="submit" className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-semibold text-lg">
                 Submit
             </button>
